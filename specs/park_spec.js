d973bb5bbb9a54e68f3bd2358083d9aeb74b7513
Simon Shephard <simonshephard@hotmail.com>
2018-09-04 16:38:55 +0100

    initial commit at hw start point

---
+++ specs/park_spec.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+const Park = require('../models/park.js');
+const Dinosaur = require('../models/dinosaur.js');
+
+describe('Park', function() {
+
+  beforeEach(function () {
+
+  })
+
+  it('should have a name');
+
+  it('should have a ticket price');
+
+  it('should have a collection of dinosaurs');
+
+  it('should be able to add a dinosaur to its collection');
+
+  it('should be able to remove a dinosaur from its collection');
+
+  it('should be able to find the dinosaur that attracts the most visitors');
+
+  it('should be able to find all dinosaurs of a particular species');
+
+  it('should be able to remove all dinosaurs of a particular species');
+
+});
