67fb48095b42f6f75c879783a1302048414f1948
Simon Shephard <simonshephard@hotmail.com>
2018-09-04 17:42:29 +0100

    adds removeDinosaur method

--- models/park.js
+++ models/park.js
@@ -8,5 +8,9 @@ Park.prototype.addDinosaur = function(dinosaur) {
   this.dinosaurs.push(dinosaur);
 };
 
+Park.prototype.removeDinosaur = function(dinosaur) {
+  let index = this.dinosaurs.indexOf(dinosaur);
+  this.dinosaurs.splice(index, 1);
+};
 
 module.exports = Park;
